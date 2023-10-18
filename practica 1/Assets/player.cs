7cc431dc37b3e01448baedd387f0fbefe8ed08fa
LeonardoLopez11 <leonardolopeztolentino@gmail.com>
2023-10-17 21:08:03 -0500

    creacion de scrips y escena

--- practica 1/Assets/player.cs
+++ practica 1/Assets/player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public float velocidad = 5; 
+    private Rigidbody rb; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0.0f, movimientoVertical);
+        rb.velocity = movimiento * velocidad;
     }
 }
